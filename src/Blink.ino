e39aefe36f4ee719377f60634caf76f98827cf7b
SId Price <sid@sidprice.com>
2022-06-15 11:08:02 -0600

    Simple test program

---
+++ src/Blink.ino
@@ -0,0 +1,23 @@
+/* Rui Santos
+  Complete instructions at https://RandomNerdTutorials.com/esp32-cam-projects-ebook/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+
+// ledPin refers to ESP32-CAM GPIO 4 (flashlight)
+const int ledPin = 4;
+
+void setup() {
+  // initialize digital pin ledPin as an output
+  Serial.begin(115200) ;
+  Serial.println("Starting") ;
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  Serial.println("On") ;
+  delay(2000);
+  Serial.println("Off") ;
+  delay(2000);
+}
